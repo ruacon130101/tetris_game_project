9706f110c33bc3f2b8381485f9eebdb8d0a3ad3c
ruacon130101 <ruacon130101@gmail.com>
2020-05-13 17:21:38 +0700

    Update runtetris.hpp

--- runtetris.hpp
+++ runtetris.hpp
@@ -7,7 +7,7 @@ class Ahihi{
       enum { W = 10, H = 20 };
       void draw(SDL_Renderer *);
       bool check(const Action &t) const;
-      void unite(const Action &);
+      void deleteLastRow(const Action &);
       void init(int x2, int y2);
     private:
       bool block[W][H];

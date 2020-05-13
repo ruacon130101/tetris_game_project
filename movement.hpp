216e10911207d23536968d694bd34014464fed30
ruacon130101 <ruacon130101@gmail.com>
2020-05-13 17:21:04 +0700

    Update movement.hpp

--- movement.hpp
+++ movement.hpp
@@ -3,15 +3,15 @@
 
 class Action{
     public:
-      enum Type { I , L, J, S, T, Z, O };
+      enum Type {I, J, L, O, S, T, Z};
       Action(Type);
       void draw(SDL_Renderer*);
+      void rotation();
       void move(int dx, int dy);
-      int checkBlock(int x, int y) const;
       int x() const;
       int y() const;
+      bool checkBlock(int x, int y) const;
     private:
       Type type1;
-      int x1;
-      int y1;
+      int x1, y1, goc;
 };

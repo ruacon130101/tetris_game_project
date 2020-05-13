aeac30c8dd585a7f15ef30bd759835033f083976
ruacon130101 <ruacon130101@gmail.com>
2020-05-13 17:21:20 +0700

    Update runtetris.cpp

--- runtetris.cpp
+++ runtetris.cpp
@@ -3,17 +3,18 @@ Ahihi::Ahihi():block{{0}}{}
 
 void Ahihi::draw(SDL_Renderer* renderer)
 {
-  SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
-  for (int x = 0; x < W; ++x)
+  SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
+  for(int x = 0;x < W;++x)
   {
-    for (int y = 0; y < H; ++y)
+    for(int y = 0;y < H;++y)
     {
-      if (block[x][y] == 1)
+      if(block[x][y] == 1)
       {
         //SDL_Rect fillRect = { SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
-        SDL_Rect fillRect = { x , y , 31, 31};
+        SDL_Rect fillRect = { x*40+1, y*40+1, 38, 38};
         SDL_RenderFillRect(renderer, &fillRect);
       }
+      else SDL_RenderDrawPoint(renderer, x*40+20, y*40+20);
     }
   }
 }
@@ -21,60 +22,45 @@ void Ahihi::draw(SDL_Renderer* renderer)
 
 bool Ahihi::check(const Action &t) const
 {
-  for (int x = 0; x < 4; ++x)
+  for(int x = 0;x < 4;++x)
   {
-    for (int y = 0; y < 4; ++y)
+    for(int y = 0;y < 4;++y)
     {
-        if (t.checkBlock(x, y) == 1)
+        if(t.checkBlock(x, y) == 1)
         {
-            if (block[t.x()+x][t.y()+y]) return 1;
-            if (t.x() + x < 0 || t.x() + x >= W || t.y() + y < 0 || t.y() + y >= H) return 1;
+            if(block[t.x()+x][t.y()+y]) return true;
+            if(t.x() + x < 0 || t.x() + x >= W || t.y() + y < 0 || t.y() + y >= H) return true;
         }
     }
   }
-  return 0;
+  return false;
 }
-void Ahihi::init(int x2, int y2)
+void Ahihi::deleteLastRow(const Action &t)
 {
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            fried[x2 + i][y2 + j] -= block[i][j];
-        }
-    }
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            fried[y2 + i][x2 + j] += block[i][j];
-        }
-    }
-}
-void Ahihi::unite(const Action &t)
-{
-  for (int x = 0; x < 4; ++x)
-  {
-      for (int y = 0; y < 4; ++y)
+      for(int x = 0;x < 4;++x)
       {
-          if (t.checkBlock(x, y)) block[t.x() + x][t.y() + y] = 1;
-          else fried[t.x()+x][t.y()+y] = 1;
+          for(int y = 0;y < 4;++y)
+          {
+              if(t.checkBlock(x, y)) block[t.x() + x][t.y() + y] = 1;
+          }
       }
-  }
-    int kt = 0;
-  for (int y = H - 1; y >= 0; --y)
-  {
-    kt = 1;
-    for (int x = 0; x < W; ++x)
-    {
-        if(!block[x][y]) kt = 0;
-        if (kt == 1)
+      int kt = 0;
+      for(int y = H - 1;y >= 0;--y)
+      {
+        kt = 1;
+        for(int x = 0;x < W;++x)
         {
-            for (int y2 = y - 1; y2 >= 0; --y2)
-                for (int x = 0; x < W; ++x) block[x][y2 + 1] = block[x][y2];
-            for(int x = 0;x < W;++x) fried[x][0] = 0;
-            for (int x = 0; x < W; ++x) block[x][0] = 0;
+            if(!block[x][y])
+            {
+                kt = 0;
+                break;
+            }
         }
-    }
-  }
+        if(kt == 1)
+        {
+            for(int y2 = y - 1;y2 >= 0;--y2)
+                for(int x = 0;x < W;++x) block[x][y2 + 1] = block[x][y2];
+            for(int x = 0;x < W;++x) block[x][0] = 0;
+        }
+      }
 }

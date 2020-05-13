f73f5a55e7e893c98bed542d2720961b9a5daaa5
ruacon130101 <ruacon130101@gmail.com>
2020-05-13 21:48:41 +0700

    Update movement.cpp

--- movement.cpp
+++ movement.cpp
@@ -1,18 +1,42 @@
 #include "movement.hpp"
 
-Action::Action(Type type):
+Action::Action(Type_Block type):
   type1(type),x1(3), y1(0), goc(0){}
-
-void Action::draw(SDL_Renderer* renderer)
-{
+void Action::draw(SDL_Renderer* renderer){
+    switch (type1)
+    {
+      case I:
+        SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
+        break;
+      case L_Reverse:
+        SDL_SetRenderDrawColor(renderer, 0xFF, 0xBF, 0xF0, 0xFF);
+        break;
+      case L:
+        SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0xFF, 0xFF);
+        break;
+      case Square:
+        SDL_SetRenderDrawColor(renderer, 0x00, 0xFF, 0xFF, 0xFF);
+        break;
+      case N_Reverse:
+        SDL_SetRenderDrawColor(renderer, 0x00, 0xFF, 0x00, 0xFF);
+        break;
+      case T:
+        SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0x00, 0xFF);
+        break;
+      case N:
+        SDL_SetRenderDrawColor(renderer, 0xFF, 0xA5, 0x00, 0xFF);
+        break;
+    }
   for (int x = 0; x < 4; ++x)
   {
       for (int y = 0; y < 4; ++y)
-      if (checkBlock(x, y))
       {
+          if (checkBlock(x, y))
+          {
         //SDL_Rect fillRect = { SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
-        SDL_Rect fillRect = {(x+x1)*40+1, (y+y1)*40+1, 38, 38};
-        SDL_RenderFillRect(renderer, &fillRect);
+            SDL_Rect fillRect = {(x+x1)*40+1, (y+y1)*40+1, 38, 38};
+            SDL_RenderFillRect(renderer, &fillRect);
+          }
       }
   }
 }
@@ -30,7 +54,7 @@ int Action::y() const{
   return y1;
 }
 bool Action::checkBlock(int x, int y) const{
-  static const char *Shapes[][4] = {
+  static const char *Tetris[][4] = {
     {
       " #  "
       " #  "
@@ -158,6 +182,6 @@ bool Action::checkBlock(int x, int y) const{
       "    ",
     },
   };
-  return Shapes[type1][goc][x+y*4] == '#';
+  return Tetris[type1][goc][x+y*4] == '#';
 }
 
